3ab9178c16c3628789fe4a950c2edb8588ebae9a
bpatwa-sanimax <bhavya.patwa@sanimax.com>
2024-07-08 18:09:54 +0000

    misc

--- .devcontainer/postStartCommand.bash
+++ .devcontainer/postStartCommand.bash
@@ -1,1 +1,7 @@
-python /workspaces/sanimax-analytics/.devcontainer/get_secrets_from_dagster_cloud.py
+# Login to Terraform Cloud
+# (https://developer.hashicorp.com/terraform/tutorials/cloud-get-started/cloud-login)
+pip install colorama
+python /workspaces/sanimax-analytics/.devcontainer/login_to_terraform_cloud.py
+
+# Initialize Terraform (https://developer.hashicorp.com/terraform/cli/commands/init)
+terraform init
