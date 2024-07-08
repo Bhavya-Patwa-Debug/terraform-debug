c62f137ee7fceadaa4fca274c83504e25876a1ee
bpatwa-sanimax <bhavya.patwa@sanimax.com>
2024-07-08 12:46:40 -0400

    Update main.py

--- tailscale/main.py
+++ tailscale/main.py
@@ -1,20 +1,36 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  cloud {
-    organization = "bhavya-debug"
-    workspaces {
-      name = "terraform-debug"
-    }
-  }
-
-  required_providers {
-    tailscale = {
-      source  = "tailscale/tailscale"
-      version = "~> 0.16.1"
-    }
-  }
+resource "tailscale_acl" "as_json" {
+  acl = jsonencode({
+    acls : [
+      {
+        // Allow all users access to all ports.
+        action : "accept",
+        users : ["*"],
+        ports : ["*:*"],
+      },
+    ],
+  })
 }
 
-provider "tailscale" {
+resource "tailscale_acl" "as_hujson" {
+  acl = <<EOF
+  {
+    // Comments in HuJSON policy are preserved when the policy is applied.
+    "acls": [
+      {
+        // Allow all users access to all ports.
+        "action" : "accept",
+        "users"  : ["*"],
+        "ports"  : ["*:*"],
+      },
+    ],
+  }
+  EOF
+}
+
+resource "tailscale_tailnet_key" "sample_key" {
+  reusable      = true
+  ephemeral     = false
+  preauthorized = true
+  expiry        = 3600
+  description   = "Sample key"
 }
