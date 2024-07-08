415a32380ba8dbc6de8f0148aa9d23c7792ddc24
bpatwa-sanimax <bhavya.patwa@sanimax.com>
2024-07-08 12:45:24 -0400

    Create main.py

---
+++ tailscale/main.py
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  cloud {
+    organization = "bhavya-debug"
+    workspaces {
+      name = "terraform-debug"
+    }
+  }
+
+  required_providers {
+    tailscale = {
+      source  = "tailscale/tailscale"
+      version = "~> 0.16.1"
+    }
+  }
+}
+
+provider "tailscale" {
+}
