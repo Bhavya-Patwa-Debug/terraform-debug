011357391059806da63d973a3fd17d3d4cc241ca
bpatwa-sanimax <bhavya.patwa@sanimax.com>
2024-07-08 12:48:39 -0400

    Create resources.tf

---
+++ tailscale/resources.tf
@@ -0,0 +1,36 @@
+resource "tailscale_acl" "as_json" {
+  acl = jsonencode({
+    acls : [
+      {
+        // Allow all users access to all ports.
+        action : "accept",
+        users : ["*"],
+        ports : ["*:*"],
+      },
+    ],
+  })
+}
+
+resource "tailscale_acl" "as_hujson" {
+  acl = <<EOF
+  {
+    // Comments in HuJSON policy are preserved when the policy is applied.
+    "acls": [
+      {
+        // Allow all users access to all ports.
+        "action" : "accept",
+        "users"  : ["*"],
+        "ports"  : ["*:*"],
+      },
+    ],
+  }
+  EOF
+}
+
+resource "tailscale_tailnet_key" "sample_key" {
+  reusable      = true
+  ephemeral     = false
+  preauthorized = true
+  expiry        = 3600
+  description   = "Sample key"
+}
